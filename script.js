141ca0d7ac148b939acfc5dd375d8bbedce51ca2
Rishabh Sharma <sharmarishabh1162@gmail.com>
2024-08-22 23:59:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,11 +42,12 @@ async function checkWeather(city){
 }
     
     
-
-    
-
-    
-
 searchBtn.addEventListener("click" , ()=>{
     checkWeather(searchBox.value);
 })
+
+searchBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
